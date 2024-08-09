ec32662d3ea45d85d5d933675c6bad4c3f45eea8
BrianErikson <briankerikson@gmail.com>
2024-08-08 20:57:00 -0600

    updated repository

--- build.sh
+++ build.sh
@@ -5,9 +5,10 @@ REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$REPO_ROOT/docs"
 
 generate_release() {
-    local dist_dir=$1
-    local bin_dir=$2
-    dpkg-scanpackages pool/ > $bin_dir/Packages
+    local pool=$1
+    local dist_dir="dists/${pool}"
+    local bin_dir="${dist_dir}/main/binary-amd64"
+    dpkg-scanpackages "pool/${pool}" > $bin_dir/Packages
     cat $bin_dir/Packages | gzip -9 > $bin_dir/Packages.gz
     apt-ftparchive release $bin_dir > $bin_dir/Release
     gpg --default-key "support@insightreactions.com" -abs -o - $bin_dir/Release > $bin_dir/Release.gpg
@@ -19,5 +20,6 @@ generate_release() {
 }
 
 # Call the function with the required parameters
-generate_release "dists/stable" "dists/stable/main/binary-amd64"
-generate_release "dists/testing" "dists/testing/main/binary-amd64"
+generate_release "stable"
+generate_release "testing"
+
